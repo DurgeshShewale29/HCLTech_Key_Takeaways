6a749eea436753eaa31855da09f8eb20590fd567
Durgesh Rajendra Shewale <135796411+DurgeshShewale29@users.noreply.github.com>
2025-05-16 22:52:47 +0530

    Delete book directory

--- book/src/main/java/com/cca/SD/book/models/MyUserDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cca.SD.book.models;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.cca.SD.book.models.AppUser;
-
-@Repository
-public interface MyUserDetailsRepository extends JpaRepository<AppUser,Long> {
-	AppUser getAppUserByEmail(String userName);
-}
--- book/src/main/java/com/cca/SD/book/repositories/BookDBRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cca.SD.book.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.cca.SD.book.models.Book;
-@Repository
-public interface BookDBRepository extends JpaRepository<Book,Long>{
-	Book getBookByBno(Long bno);
-}
